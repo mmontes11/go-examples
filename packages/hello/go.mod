596dbf79b4f25d44eaa07f1d580e90430221a463
Martín Montes <martinmp@ext.inditex.com>
2020-05-07 23:13:31 +0200

    Added examples

--- packages/hello/go.mod
+++ packages/hello/go.mod
@@ -1,3 +1,8 @@
 module github.com/mmontes11/go-examples/packages/hello
 
 go 1.14
+
+require (
+	github.com/google/go-cmp v0.4.0
+	github.com/mmontes11/go-examples/packages/strings v0.0.0-20200507205150-46703375fd82
+)
--- packages/hello/hello.go
+++ packages/hello/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/mmontes11/go-examples/packages/strings"
 )
 
 func main() {
-	fmt.Println(strings.Reverse("Hello, world."))
+	fmt.Println(strings.Reverse("Hello, world"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
