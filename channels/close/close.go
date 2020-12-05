a3a65ce5e659bb5f6095ab6186ab7a7c84af2ac9
Martín Montes <martin11lrx@gmail.com>
2020-12-05 19:35:23 +0100

    Added channel close example

---
+++ channels/close/close.go
@@ -0,0 +1,42 @@
+package main
+
+import "log"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				log.Printf("Received job %d", j)
+			} else {
+				log.Print("No more jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+	for i := 0; i < 10; i++ {
+		log.Printf("Sending job %d...", i)
+		jobs <- i
+	}
+	close(jobs)
+
+	<-done
+
+	jobs = make(chan int, 5)
+	log.Println()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			log.Printf("Sending job %d...", i)
+			jobs <- i
+		}
+		close(jobs)
+	}()
+	for j := range jobs {
+		log.Printf("Received job %d...", j)
+	}
+}
